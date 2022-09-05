b206df651caebad2343daab9958a49cccbf6311f
Joshua Hagan <joshua.hagan@opencareinitiative.org>
2022-09-05 12:34:44 +0000

    Modify Authentication Files

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
+    Route::get('/', function () {
         return view('dashboard');
     })->name('dashboard');
+    
 });
